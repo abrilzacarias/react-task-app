43f96d94f5b7d11d8509208a60417e2910a58122
Abril Zacarias <abrilzacarais6@gmail.com>
2024-07-24 20:59:04 -0300

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+    {
+        id:0,
+        title: '1 tarea',
+        description: '1 tarea'
+    },
+    {
+        id:1,
+        title: '2 tarea',
+        description: '2 tarea'
+    },
+    {
+        id:3,
+        title: '3 tarea',
+        description: '3 tarea'
+    },
+    {
+        id:4,
+        title: '4 tarea',
+        description: '4 tarea'
+    }
+]
